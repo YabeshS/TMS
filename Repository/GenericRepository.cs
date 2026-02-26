23409f55898793e3deb8d8623decf54d5e8e8832
yabeshlivins <yabesh.net@gmail.com>
2026-02-27 02:11:55 +0530

    Add project files.

---
+++ Repository/GenericRepository.cs
@@ -0,0 +1,207 @@
+﻿
+using System.Linq.Expressions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Repository.Models;
+
+namespace TMS.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+
+        public GenericRepository(TMSContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+
+        public async Task<T> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await SaveAsync();
+        }
+
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await SaveAsync();
+        }
+
+
+        public async Task DeleteAsync(object id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await SaveAsync();
+            }
+        }
+
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await SaveAsync();
+        }
+
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.AnyAsync(predicate);
+        }
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.CountAsync(predicate);
+        }
+
+
+
+        public async Task Add(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            // await SaveAsync();
+        }
+
+
+
+        public async Task Update(T entity)
+        {
+            _dbSet.Update(entity);
+            //await SaveAsync();
+        }
+
+        public async Task Save(T entity)
+        {
+            // _dbSet.Update(entity);
+            await SaveAsync();
+        }
+
+
+
+
+
+
+
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+            await _context.SaveChangesAsync();
+        }
+        public async Task AddRange(IEnumerable<T> entities)
+        {
+            await _context.Set<T>().AddRangeAsync(entities);
+            //await _context.SaveChangesAsync();
+        }
+
+
+
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null)
+        {
+            try
+            {
+                IQueryable<T> query = _dbSet;
+
+                if (filter != null)
+                {
+                    query = query.Where(filter);
+                }
+
+                if (include != null)
+                {
+                    query = include(query);
+                }
+
+                return await query.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return await _dbSet.FirstOrDefaultAsync(predicate);
+            }
+            catch (Exception ex)
+            {
+                throw; // Re-throw the exception for handling at a higher level
+            }
+        }
+
+
+
+        public async Task UpdateRangeAsync(IEnumerable<T> entities)
+        {
+            _dbSet.UpdateRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task RemoveAllAsync<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class
+        {
+            try
+            {
+                var entities = await _context.Set<TEntity>().Where(predicate).ToListAsync();
+
+                if (entities.Any())
+                {
+                    _context.Set<TEntity>().RemoveRange(entities);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or handle the exception
+                throw new InvalidOperationException("Error occurred while removing records.", ex);
+            }
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            if (entities == null || !entities.Any()) return;
+
+            _context.RemoveRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
---
+++ Repository/IGenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+
+using System.Linq.Expressions;
+
+namespace TMS.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<T> GetByIdAsync(object id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task UpdateAsync(T entity);
+        Task AddAsync(T entity);
+        Task AddRange(IEnumerable<T> entities);
+
+        Task UpdateRangeAsync(IEnumerable<T> entities);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+        Task DeleteAsync(object id);
+        Task DeleteAsync(T entity);
+        Task SaveAsync();
+        Task RemoveAllAsync<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : class;
+
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+
+
+
+
+    }
+}
